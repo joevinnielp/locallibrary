23282b0df19bdda3d77fbb166d467e3a4dc61922
Louis <louisaleksieb.dagusen@gmail.com>
2017-09-06 10:42:23 +0800

    added index template and static files

--- catalog/urls.py
+++ catalog/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-	#url(r'^$', views.home, name='home')
+	url(r'^$', views.index, name='index'),
 ]
