4a5c7c70013ba9202d215c8cea96dbd0fa9ba64f
Louis <louisaleksieb.dagusen@gmail.com>
2017-09-11 12:51:06 +0800

    fixed navbar active

--- catalog/static/js/custom.js
+++ catalog/static/js/custom.js
@@ -1,6 +1,8 @@
-$('.nav  li').click(function(e) {
-  //console.log(e.currentTarget,"//",e.target,"//",this);
-  e.stopPropagation();
-  $('.nav li').removeClass('active');
-  $(this).addClass('active');
-});
+var url = window.location;
+// Will only work if string in href matches with location
+$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+// Will also work for relative and absolute hrefs
+$('ul.nav a').filter(function() {
+    return this.href == url;
+}).parent().addClass('active');
